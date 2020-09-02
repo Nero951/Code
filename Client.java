40a4faaec31d0759f5309f31c98502c581551237
Nero951 <zhlwd103511@gmail.com>
2020-09-02 22:21:24 +0800

    Client

---
+++ Client.java
@@ -0,0 +1,12 @@
+import java.until.*;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        tre(Socket socket = new Socket("14.215.177.39",443)){
+            scanner.nextLine();
+        }
+    }
+}
