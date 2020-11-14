26fe897b8792313d18aaf462e4abe80cdd1e2cb7
Nero951 <zhlwd103511@gmail.com>
2020-11-14 20:47:22 +0800

    lucky-draw

--- lucky-draw/src/main/java/nero/base/BaseMapper.java
+++ lucky-draw/src/main/java/nero/base/BaseMapper.java
@@ -2,7 +2,12 @@ package nero.base;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BaseMapper<T extends BaseEntity> {
-
+    T selectByPrimaryKey(Integer id);
+    int deleteByPrimaryKey(Integer id);
+    List<T> selectAll();
+    List<T> selectByCondition(T t);
 }
