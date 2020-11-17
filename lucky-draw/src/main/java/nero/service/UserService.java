82072bf67eb606a87f20128af5f6f63cc6e18edd
Nero951 <zhlwd103511@gmail.com>
2020-11-17 21:54:09 +0800

    lucky-draw

--- lucky-draw/src/main/java/nero/service/UserService.java
+++ lucky-draw/src/main/java/nero/service/UserService.java
@@ -12,9 +12,17 @@ public class UserService {
     private UserMapper userMapper;
 
     public User login(User user) {
-        User exits = userMapper.login(user);
+//        User exits = userMapper.login(user);
+//        if(exits == null)
+//            throw new ClientException("user001", "用户名密码校验失败！");
+        User query = new User();
+        query.setUsername(user.getUsername());
+        User exits = userMapper.login(query);
         if(exits == null)
-            throw new ClientException("user001", "用户名密码校验失败！");
+            throw new ClientException("user001", "用户不存在！");
+        if(!exits.getPassword().equals(user.getPassword()))
+            throw new ClientException("user002","用户名密码校验失败！");
         return exits;
+
     }
 }
