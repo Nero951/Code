4bb2c673663b731e9149717cc84242e4d1900cad
Nero951 <zhlwd103511@gmail.com>
2020-11-28 23:35:51 +0800

    ReviewTest

--- ReviewTest/Test1/src/MainTest3.java
+++ ReviewTest/Test1/src/MainTest3.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class MainTest3 {
-//    public static void main1(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        while(scan.hasNext()){
-//            String str1 = scan.nextLine();
-//            String str2 = scan.nextLine();
-//            String pattern = "[" + str2 + "]";
-//            String result = str1.replaceAll(pattern, "");
-//            System.out.println(result);
-//        }
-//    }
+    public static void main1(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String str1 = scan.nextLine();
+            String str2 = scan.nextLine();
+            String pattern = "[" + str2 + "]";
+            String result = str1.replaceAll(pattern, "");
+            System.out.println(result);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
---
+++ ReviewTest/Test1/src/MainTest4.java
@@ -0,0 +1,11 @@
+class Gift{
+    int number;
+    int data;
+    public Gift(int number, int data) {
+        this.number = number;
+        this.data = data;
+    }
+}
+public class MainTest4 {
+
+}
