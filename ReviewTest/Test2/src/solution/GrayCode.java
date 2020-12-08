801a2acf5a648d2bfc9c6fa65201b0d90caceff2
Nero951 <zhlwd103511@gmail.com>
2020-12-08 21:14:08 +0800

    ReviewTest

--- ReviewTest/Test2/src/solution/GrayCode.java
+++ ReviewTest/Test2/src/solution/GrayCode.java
@@ -2,16 +2,15 @@ package solution;
 
 public class GrayCode {
     public String[] getGray(int n) {
-        // write code here
         String[] str = null;
         if(n==1){
             str = new String[]{"0","1"};
         }else{
             String[] temp = getGray(n-1);
             str = new String[2*temp.length];
-            for (int i = 0; i<str.length ; i++) {
-                str[i] = "0" + str[i];
-                str[str.length-1-i] = "1" + str[i];
+            for (int i = 0; i<temp.length ; i++) {
+                str[i] = "0" + temp[i];
+                str[str.length-1-i] = "1" + temp[i];
             }
         }
         return str;
